ee895f63117fcc045a6a0a41800f1778677e0cdf
Aidan <111407481+amusasrd@users.noreply.github.com>
2023-01-22 22:16:31 +1100

    Add files via upload

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by A Moses on 19/1/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
